576953bb95324670e25b5e687f84448190eac43f
Juan Fernandez <jcfernan@live.com>
2020-04-22 20:04:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while count < src.length do
     p src [count] [inner_count]
     inner_count += 1
   end
-  
+  count += 1
 end
   
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
