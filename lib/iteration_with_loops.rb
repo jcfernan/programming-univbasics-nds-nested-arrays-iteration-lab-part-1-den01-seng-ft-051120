9a355c05d54c1592861c033ad3ebce8feb914cfe
Juan Fernandez <jcfernan@live.com>
2020-04-22 20:01:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,9 @@ def find_even_values(src)
   count = 0
   
 while count > src.length do
+  inner_count = 0
   
+  while 
   
   
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
